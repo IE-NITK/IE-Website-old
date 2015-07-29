c9737a62ba349c63eb2be805cccf0c115ae7543c
jaidevanoop <jaidev.anoop@gmail.com>
2015-07-29 14:38:19 +0530

    addition of form

--- ie-site.js
+++ ie-site.js
@@ -1,5 +1,41 @@
+Person = new Mongo.Collection("persons");
+Person.attachSchema(new SimpleSchema({
+  name: {
+    type: String,
+    label: "Name",
+    max: 200
+  },
+  branch: {
+    type: String,
+    label: "Branch"
+  },
+  contact: {
+    type: String,
+    label: "Contact Number",
+    //min: 0
+  },
+  dob: {
+    type: String,
+    label: "Date of birth",
+    optional: true
+  },
+  summary: {
+    type: String,
+    label: "About me",
+    optional: true,
+    max: 1000
+  }
+}));
+
 if (Meteor.isClient) {
+
+
 }
+
+
+
+
+
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
@@ -16,3 +52,7 @@ Router.route('/form');
 Router.route('/sigs');
 
 Router.route('/events');
+
+
+
+
