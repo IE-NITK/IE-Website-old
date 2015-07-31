2bd797f0b91127f9a85b0cc6eb5aa9056526f80a
jaidevanoop <jaidev.anoop@gmail.com>
2015-07-31 16:48:07 +0530

    restructure

--- ie-site.js
+++ ie-site.js
@@ -29,6 +29,49 @@ Person.attachSchema(new SimpleSchema({
 
 if (Meteor.isClient) {
 
+Template.register.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Accounts.createUser({
+    email: email,
+    password: password
+}, function(error){
+    if(error){
+        console.log(error.reason); // Output error if registration fails
+    } else {
+        Router.go("home"); // Redirect user if registration succeeds
+    }
+});
+
+        Router.go('/');
+    }});
+Template.navbar.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+        Router.go('login');
+    }
+});
+
+Template.login.events({
+    'submit form': function(event){
+        event.preventDefault();
+        var email = $('[name=email]').val();
+        var password = $('[name=password]').val();
+        Meteor.loginWithPassword(email, password, function(error){
+    if(error){
+        console.log(error.reason);
+    } else {
+        Router.go("/");
+    }
+});
+
+    }
+});
+
+
 
 }
 
@@ -52,6 +95,10 @@ Router.route('/form');
 Router.route('/sigs');
 
 Router.route('/events');
+
+Router.route('/register');
+
+Router.route("/login");
 
 
 
